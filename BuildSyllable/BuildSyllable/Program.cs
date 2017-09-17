473ec923e30eb2aa802162ead58a2a4a8e266849
Krzysztof Wojciechowski <krzysztof.wojciechowski@capgamini.com>
2017-09-17 14:12:43 +0200

    Build syllables beginning

---
+++ BuildSyllable/BuildSyllable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerginCharakters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] samogloski = new string[6] { "a", "e", "i", "o", "u", "y" };
+            string[] spolgloski = new string[18] {"b", "c", "d", "f", "g", "h",
+                                                  "j", "k", "l", "l", "m", "n",
+                                                  "p", "r", "s", "t", "w", "y"};
+
+            string sylaba;
+
+            sylaba = samogloski[0] + spolgloski[0];
+
+            Console.WriteLine("sylaba: " + sylaba);
+
+        }
+    }
+}
