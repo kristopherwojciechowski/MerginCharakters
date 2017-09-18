b941777f0b598945a80f913203c98248f9a166d3
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-09-18 20:42:56 +0200

    for 2 letters works fine ;)

--- BuildSyllable/BuildSyllable/Program.cs
+++ BuildSyllable/BuildSyllable/Program.cs
@@ -11,23 +11,29 @@ namespace MerginCharakters
         static void Main(string[] args)
         {
             string[] samogloski = new string[6] { "a", "e", "i", "o", "u", "y" };
-            string[] spolgloski = new string[24] {"a", "b", "c", "d", "e", "f", "g", "h",
-                                                  "i", "j", "k", "l", "ł", "m", "n", "o",
-                                                  "p", "r", "s", "t", "u", "w", "y", "z"};
+            //string[] spolgloski = new string[24] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y", "z"};
+            string[] spolgloski = new string[18] {"b", "c", "d", "f", "g", "h", "j", "k", "l", "ł", "m", "n", "p", "r", "s", "t", "w", "z"};
 
             string[,,] sylaba = new string[samogloski.Length,spolgloski.Length,2];
 
-            int i, j;
             string decision;
             Random rand = new Random();
-            int k = 3;
 
             do
             {
-                int indexSamogloski = rand.Next(samogloski.Length);
-                int indexSpolgloski = rand.Next(spolgloski.Length + 1);
+                Console.WriteLine();
 
-                string letter1 = spolgloski.GetValue(indexSamogloski);
+                int indexSamogloski = rand.Next(samogloski.Length);
+                int indexSpolgloski = rand.Next(spolgloski.Length);
+                string letter1 = spolgloski[indexSpolgloski];
+                string letter2 = samogloski[indexSamogloski];
+
+                Console.WriteLine(letter1 + letter2);
+                Console.WriteLine();
+                Console.WriteLine(letter2 + letter1);
+                Console.WriteLine();
+
+
 
                 Console.WriteLine("If you want to continue press n ");
                 Console.WriteLine("If you want to leave press e ");
