c2355e0b99de17ba919e54cb01df58636343612a
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-09-18 20:52:33 +0200

    starts building methodes

--- BuildSyllable/BuildSyllable/Syllables.cs
+++ BuildSyllable/BuildSyllable/Syllables.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MerginCharakters
+namespace BuildSyllable
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Syllables
+    { 
+    
+        public static String BuildSyllableswithTwoLetters()
         {
             string[] samogloski = new string[6] { "a", "e", "i", "o", "u", "y" };
             //string[] spolgloski = new string[24] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y", "z"};
-            string[] spolgloski = new string[18] {"b", "c", "d", "f", "g", "h", "j", "k", "l", "ł", "m", "n", "p", "r", "s", "t", "w", "z"};
+            string[] spolgloski = new string[18] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "ł", "m", "n", "p", "r", "s", "t", "w", "z" };
+
+            //string[,,] sylaba = new string[samogloski.Length, spolgloski.Length, 2];
 
             string decision;
             Random rand = new Random();
+            string syllable;
 
             do
             {
@@ -31,10 +35,14 @@ namespace MerginCharakters
                 Console.WriteLine(letter2 + letter1);
                 Console.WriteLine();
 
+                syllable = letter1 + letter2;
+
                 Console.WriteLine("If you want to continue press n ");
                 Console.WriteLine("If you want to leave press e ");
 
                 decision = Console.ReadLine();
+                syllable = letter1 + letter2;
+                return syllable;
 
             } while (decision != "e");
         }
