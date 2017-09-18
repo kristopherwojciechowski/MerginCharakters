244c1dd6906a5c52dc0eae486e83ee23cea5b9d4
Krzysztof Wojciechowski <krzysztof.wojciechowski@capgamini.com>
2017-09-18 09:54:34 +0200

    utknalem z getvalue

--- BuildSyllable/BuildSyllable/Program.cs
+++ BuildSyllable/BuildSyllable/Program.cs
@@ -18,21 +18,36 @@ namespace MerginCharakters
             string[,,] sylaba = new string[samogloski.Length,spolgloski.Length,2];
 
             int i, j;
+            string decision;
+            Random rand = new Random();
+            int k = 3;
 
-            for (i=0; i<samogloski.Length; i++)
+            do
             {
-                for(j=0; j<spolgloski.Length; j++)
-                {
-                    if (samogloski[i] != spolgloski[j])
-                    {
-                        sylaba[i, j, 0] = samogloski[i] + spolgloski[j];
-                        sylaba[i, j, 1] = spolgloski[j] + samogloski[i];
-                    }
+                Int32 indexSamogloski = rand.Next(samogloski.Length);
+                int indexSpolgloski = rand.Next(spolgloski.Length + 1);
+                string letter1 = spolgloski.GetValue(indexSamogloski);
+                Console.WriteLine("If you want to continue press n ");
+                Console.WriteLine("If you want to leave press e ");
 
-                    Console.WriteLine(sylaba[i, j, 0] + " " + sylaba[i, j, 1]);
+                decision = Console.ReadLine();
 
-                }
-            }
+            } while (decision != "e");
+
+            //for (i=0; i<samogloski.Length; i++)
+            //{
+            //    for(j=0; j<spolgloski.Length; j++)
+            //    {
+            //        if (samogloski[i] != spolgloski[j])
+            //        {
+            //            sylaba[i, j, 0] = samogloski[i] + spolgloski[j];
+            //            sylaba[i, j, 1] = spolgloski[j] + samogloski[i];
+            //        }
+
+            //        Console.WriteLine(sylaba[i, j, 0] + " " + sylaba[i, j, 1]);
+
+            //    }
+            //}
         }
     }
 }
