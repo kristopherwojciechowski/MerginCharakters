7b5f9325091d97c362b5079e3917d0736e3e654c
Krzysztof Wojciechowski <krzysztof.wojciechowski@capgamini.com>
2017-09-23 19:32:00 +0200

    work fine but need improvment

--- BuildSyllable/EnglishNumbers/Program.cs
+++ BuildSyllable/EnglishNumbers/Program.cs
@@ -11,40 +11,50 @@ namespace EnglishNumbers
         static void Main(string[] args)
         {
             string[] liczebnikiSlownie = new string[5] { "one", "two", "three", "four", "five" };
-            int[] liczebnikiCyframi = new int[5] { 1, 2, 3, 4, 5};
+            int[] liczebnikiCyframi = new int[5] { 1, 2, 3, 4, 5 };
             string decision;
+            string wpisanyTekst;
+            int wylosowanaIndex;
+            string wylosowanyLiczebnikSlowo;
+            int wylosowanyLiczebnikNumer;
+
+
             do
             {
                 Random rand = new Random();
 
-                int wylosowanaIndex = rand.Next(liczebnikiCyframi.Length);
+                wylosowanaIndex = rand.Next(liczebnikiCyframi.Length);
 
-                Console.WriteLine("wyslosawana liczba to " + liczebnikiCyframi[wylosowanaIndex] + " -> " + "(" + liczebnikiSlownie[wylosowanaIndex] + ")");
+                //Console.WriteLine("wyslosawana liczba to " + liczebnikiCyframi[wylosowanaIndex] + " -> " + "(" + liczebnikiSlownie[wylosowanaIndex] + ")");
 
+                wylosowanyLiczebnikSlowo = liczebnikiSlownie[wylosowanaIndex];
+                wylosowanyLiczebnikNumer = liczebnikiCyframi[wylosowanaIndex];
+
+                Console.WriteLine("Wylosowana liczba to {0}", wylosowanyLiczebnikNumer);
+                Console.WriteLine("Wpisz slownie podany liczebnik:");
+
+                wpisanyTekst = Console.ReadLine();
+
+                if (wpisanyTekst == wylosowanyLiczebnikSlowo)
+                {
+                    Console.WriteLine("Poprawna pisowania");
+                }
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine("Popraw pisowanie");
+                        wpisanyTekst = Console.ReadLine();
+
+                    } while (wpisanyTekst != wylosowanyLiczebnikSlowo);
+                }
+                Console.WriteLine("Poprawna pisowania");
                 Console.WriteLine("If you want to continue press n ");
                 Console.WriteLine("If you want to leave press e ");
 
                 decision = Console.ReadLine();
-
-
-            string wylosowanyLiczebnikSlowo = liczebnikiSlownie[wylosowanaIndex];
-            int wylosowanyLiczebnikNumer = liczebnikiCyframi[wylosowanaIndex];
-
-            Console.WriteLine("Wylosowana liczba to {0}", wylosowanyLiczebnikNumer);
-            Console.WriteLine("Wpisz slownie podany liczebnik:");
-
-            string wpisanyTekst = Console.ReadLine();
-
-            if(wpisanyTekst == wylosowanyLiczebnikSlowo)
-            {
-                Console.WriteLine("Poprawna pisowania");
             }
-            else
-            {
-                Console.WriteLine("Popraw pisowanie");
-
-            }
-        } while (decision != "e");
+            while (decision != "e");
 
         }
     }
