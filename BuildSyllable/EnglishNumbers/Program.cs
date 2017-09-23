f87ebc897bf0462f5c142319edb8cf599c33da22
Krzysztof Wojciechowski <krzysztof.wojciechowski@capgamini.com>
2017-09-23 15:28:05 +0200

    new application

---
+++ BuildSyllable/EnglishNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] liczebnikiSlownie = new string[5] { "one", "two", "three", "four", "five" };
+            int[] liczebnikiCyframi = new int[5] { 1, 2, 3, 4, 5};
+            string decision;
+            do
+            {
+                Random rand = new Random();
+
+                int wylosowanaIndex = rand.Next(liczebnikiCyframi.Length);
+
+                Console.WriteLine("wyslosawana liczba to " + liczebnikiCyframi[wylosowanaIndex] + " -> " + "(" + liczebnikiSlownie[wylosowanaIndex] + ")");
+
+                Console.WriteLine("If you want to continue press n ");
+                Console.WriteLine("If you want to leave press e ");
+
+                decision = Console.ReadLine();
+
+            } while (decision != "e");
+            
+
+
+        }
+    }
+}
